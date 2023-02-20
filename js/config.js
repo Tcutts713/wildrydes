9e5995dd168ec26005e76c670038fe549c0ddcd9
Tim Cutts <78920662+Tcutts713@users.noreply.github.com>
2023-02-20 09:03:01 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_mHN7IxSIJ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7s29r105hj34qoodneohelcnda', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '37tlo2d9562df3e8htc8n9j8p7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
