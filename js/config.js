e80a5af80c5a7c1038cfb7aaab139322e85ffcd4
Tim Cutts <78920662+Tcutts713@users.noreply.github.com>
2023-02-20 08:54:16 -0800

    Update config.js
    added endpoints for config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_tMWgosiTJ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3i11pr476pi5u8ktm05voqj6qj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_mHN7IxSIJ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7s29r105hj34qoodneohelcnda', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://a31fq23xz7.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://4yzswfrbx8.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
